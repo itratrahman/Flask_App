d00fb5dbd6ef766fda13feaca6628c9a089b7628
Itrat Rahman <rahmanitrat@gmail.com>
2017-02-08 21:05:12 +0600

    added geocode template

--- forms.py
+++ forms.py
@@ -1,11 +1,26 @@
 ####Setting up the form backend
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
 
 class SignupForm(Form):
 
-    first_name = StringField('First name')
-    last_name = StringField('Last name')
-    email = StringField('Email')
-    password = PasswordField('Password')
+    first_name = StringField('First name', validators = [DataRequired("Please enter your first name.")])
+    last_name = StringField('Last name', validators = [DataRequired("Please enter your last name.")])
+    email = StringField('Email', validators = [DataRequired("Please enter your email."), Email("Please enter valid email address")])
+    password = PasswordField('Password', validators = [DataRequired("Please enter your password."), Length(min = 6, message = "Password must be six characters or more.")])
     submit = SubmitField('Sign up')
+
+class LoginForm(Form):
+
+    email = StringField('Email', validators = [ DataRequired("Please enter your email."), Email("Please enter valid email address")])
+
+    password = PasswordField('Password', validators = [DataRequired("Please enter your password."), Length(min = 6, message = "Password must be six characters or more.")])
+
+    submit = SubmitField('Sign in')
+
+class AddressForm(Form):
+
+    address = StringField('Address', validators = [DataRequired("Please enter an address.")])
+
+    submit = SubmitField("Search")
--- routes.py
+++ routes.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
 ##importing the db object from models.py
-from models import db
+from models import db, User, Place
 
 ##Importing the signup form class from the python file forms
-from forms import SignupForm
+from forms import SignupForm, LoginForm, AddressForm
 
 ##Creating a flask app instance
 app = Flask(__name__)
 
 ##Configuring the database of the app
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:tarti@localhost/learningflask'
 app.secret_key = "development-key"
 
 db.init_app(app) ##initialize the flask app for using the database setup
@@ -25,13 +25,122 @@ def about():
     return render_template("about.html")
 
 #Creating a route for the signup page
-@app.route("/signup")
+@app.route("/signup", methods = ['GET', 'POST'])
 def signup():
+
+    if 'email' in session:
+
+        return redirect(url_for('home'))
 
     ##Creating an object of signup form
     form = SignupForm()
 
-    return render_template('signup.html', form=form)
+    if request.method == 'POST':
+
+        if form.validate() == False:
+
+            return render_template('signup.html', form=form)
+
+        else:
+
+            ##Creating a new user
+            newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+            db.session.add(newuser)
+            db.session.commit()
+
+            ##Creating a new session
+            session['email'] = newuser.email
+            return redirect(url_for('home'))
+
+
+            return "Success!"
+
+    elif request.method == 'GET':
+
+        return render_template('signup.html', form=form)
+
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if 'email' in session:
+
+        return redirect(url_for('home'))
+
+    form = LoginForm()
+
+    if request.method == "POST":
+
+        if form.validate() == False:
+
+          return render_template("login.html", form=form)
+
+        else:
+
+            email = form.email.data
+            password = form.password.data
+            user = User.query.filter_by(email=email).first()
+
+            if user is not None and user.check_password(password):
+
+                session['email'] = form.email.data
+
+                return redirect(url_for('home'))
+
+            else:
+
+                return redirect(url_for('login'))
+
+    elif request.method == 'GET':
+
+        return render_template('login.html', form=form)
+
+
+
+@app.route("/logout")
+def logout():
+
+    session.pop('email', None)
+
+    return redirect(url_for('index'))
+
+
+@app.route("/home", methods=["GET", "POST"])
+def home():
+
+    if 'email' not in session:
+
+        return redirect(url_for('login'))
+
+    form = AddressForm()
+
+    places = []
+
+    my_coordinates = (37.4221, -122.0844)
+
+    if request.method == 'POST':
+
+        if form.validate() == False:
+
+            return render_template('home.html', form = form)
+
+        else:
+
+            # get the address
+            address = form.address.data
+
+            # query for places around it
+            p = Place()
+            my_coordinates = p.address_to_latlng(address)
+            places = p.query(address)
+
+            # return those results
+            return render_template('home.html', form=form, my_coordinates=my_coordinates, places=places)
+
+    elif request.method == 'GET':
+
+        return render_template('home.html', form=form, my_coordinates=my_coordinates, places=places)
 
 
 ##The main method runs the app
